f36d409edf4da0fbc391845304b2d601d7ca8372
marteabra <marte.abrahams1@hotmail.com>
2022-03-28 13:25:30 +0200

    First commit, layout finished frontpage

---
+++ src/components/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="footer">
+    <section class="footer__info">
+      <div class="copyright">6sneakers©</div>
+      <div>help@6sneakers.com</div>
+      <div>+47 987 65 432</div>
+    </section>
+    <section class="footer__socials">
+      <div>
+        <img
+          src="public/images/facebook-2.png"
+          alt="logo-facebook"
+          class="facebook"
+        />
+      </div>
+      <div>
+        <img
+          src="public/images/instagram-3.png"
+          alt="logo-instagram"
+          class="instagram"
+        />
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.footer {
+  width: 100vw;
+  background: white;
+  padding: 25px;
+  border-radius: 20px 20px 0 0;
+  display: flex;
+  justify-content: space-between;
+}
+
+.copyright {
+  margin-bottom: 10px;
+}
+
+.footer__socials {
+  display: flex;
+}
+
+.footer__socials img {
+  height: 30px;
+  width: 30px;
+  margin: 5px;
+}
+</style>>
---
+++ src/views/Home.vue
@@ -0,0 +1,140 @@
+<template>
+  <Header />
+  <main class="frontpage">
+    <section class="frontpage__buttons">
+      <button>
+        <svg
+          width="10"
+          height="7"
+          viewBox="0 0 10 7"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line y1="0.5" x2="10" y2="0.5" stroke="black" />
+          <line x1="2" y1="3.5" x2="10" y2="3.5" stroke="black" />
+          <line x1="4" y1="6.5" x2="10" y2="6.5" stroke="black" />
+        </svg>
+        Sort
+      </button>
+      <button>
+        Filter
+        <svg
+          width="10"
+          height="7"
+          viewBox="0 0 10 7"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line y1="1.5" x2="10" y2="1.5" stroke="black" />
+          <line y1="5.5" x2="10" y2="5.5" stroke="black" />
+          <circle cx="2.5" cy="1.5" r="1.5" fill="black" />
+          <circle cx="7.5" cy="5.5" r="1.5" fill="black" />
+        </svg>
+      </button>
+    </section>
+    <section class="frontpage__products">
+      <div class="product">
+        <div class="product__image">Image container</div>
+        <div class="product__brand">Brand</div>
+        <div class="product__product">Product Name</div>
+        <div class="product__price">Price</div>
+      </div>
+      <div class="product">
+        <div class="product__image">Image container</div>
+        <div class="product__brand">Brand</div>
+        <div class="product__product">Product Name</div>
+        <div class="product__price">Price</div>
+      </div>
+      <div class="product">
+        <div class="product__image">Image container</div>
+        <div class="product__brand">Brand</div>
+        <div class="product__product">Product Name</div>
+        <div class="product__price">Price</div>
+      </div>
+      <div class="product">
+        <div class="product__image">Image container</div>
+        <div class="product__brand">Brand</div>
+        <div class="product__product">Product Name</div>
+        <div class="product__price">Price</div>
+      </div>
+      <div class="product">
+        <div class="product__image">Image container</div>
+        <div class="product__brand">Brand</div>
+        <div class="product__product">Product Name</div>
+        <div class="product__price">Price</div>
+      </div>
+      <div class="product">
+        <div class="product__image">Image container</div>
+        <div class="product__brand">Brand</div>
+        <div class="product__product">Product Name</div>
+        <div class="product__price">Price</div>
+      </div>
+    </section>
+  </main>
+  <Footer />
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+
+  data() {
+    return {
+      appName: import.meta.env.VITE_APP_NAME,
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  font: var(--font-family);
+  color: var(--secondary-color);
+}
+
+button {
+  background: white;
+  padding: 10px;
+  font-family: var(--button-font);
+  width: 40vw;
+}
+
+.frontpage {
+  margin: 25px;
+}
+
+.product__image {
+  height: 65%;
+  background: grey;
+  border-radius: 20px 20px 0 0;
+  text-align: center;
+}
+.frontpage__products {
+  margin-top: 20px;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  column-gap: 10px;
+  row-gap: 20px;
+}
+
+.product {
+  height: 25vh;
+  width: 40vw;
+  background: white;
+  box-shadow: var(--box-shadow);
+  border-radius: 20px;
+}
+
+.frontpage__buttons {
+  display: flex;
+  justify-content: space-evenly;
+  margin-right: -20px;
+  margin-left: -20px;
+}
+</style>
